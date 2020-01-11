0a680c7db9cc9244f7222ed8daa7071a3fdaf3b0
weimo <1585558481@qq.com>
2020-01-11 20:34:51 +0800

    vuex状态管理token 完成

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as auth from '@/utils/auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  // 放置数据， 初始化的时候直接将用户信息给我们的公共状态
   state: {
+    user: auth.getUser()
   },
+  // 同步修改数据
   mutations: {
+    updateUser (state, payload) {
+      state.user = payload.user // 更新数据
+      auth.setUser(payload.user) // 将数据同步设置到本地存储中
+    }
+  },
+  // 清空User
+  clearUser () {
+    auth.delUser()
   },
   actions: {
   },
