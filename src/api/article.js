849e715fd4736b5e79e31ae504dbbe4d9161a23e
weimo <1585558481@qq.com>
2020-01-20 20:46:10 +0800

    封装渲染列表的方法

---
+++ src/api/article.js
@@ -0,0 +1,11 @@
+// 封装用于文章数据的获取
+
+import request from '@/utils/request'
+
+// 导出一个方法
+export function getArticles (params) {
+  return request({
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    params: { with_top: 1, ...params }
+  })
+}
